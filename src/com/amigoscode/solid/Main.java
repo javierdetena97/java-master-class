62ae840ba35b110f36fccd7ed77c6028ecb24487
Javier de Tena <jtena@t2client.com>
2024-03-02 18:26:24 +0100

    single responsability

--- src/com/amigoscode/solid/Main.java
+++ src/com/amigoscode/solid/Main.java
@@ -6,13 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         AreaCalculator areaCalculator = new AreaCalculator();
+        ShapesPrinter shapesPrinter = new ShapesPrinter();
         Circle circle = new Circle(10);
         Square square = new Square(10);
 
         List<Object> shapes = List.of(circle, square);
         int sum = areaCalculator.sum(shapes);
 
-        System.out.println("sum = " + sum);
+        System.out.println(shapesPrinter.json(sum));
+        System.out.println(shapesPrinter.csv(sum));
     }
 
 }
